2101975b96f1d50377a4c12f3c6f2b1bf3e6b8b4
Cezary <dajjedzenie@gmail.com>
2015-01-28 19:11:51 -0800

    rm packaging

--- BlottoAgent/src/blotto/actions/BlottoAgentAction.java
+++
@@ -1,7 +0,0 @@
-package blotto.actions;
-
-import jade.content.AgentAction;
-
-public class BlottoAgentAction implements AgentAction {
-	private static final long serialVersionUID = 8798665797533601732L;
-}
